3e7a65a997578cfc9f35ce6f9588ece22f6d827d
Thi Thu Ha Nguyen <tnguy655@my.centennialcollege.ca>
2019-09-21 00:49:57 -0400

    Add project files.

---
+++ Assignment01_Receipes/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment01_Receipes.Controllers
+{
+    public class HomeController :Controller
+    {
+        public ViewResult Home()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good night" ;
+            return View();
+        }
+        public ViewResult AddRecipe()
+        {
+            return View();
+        }
+    }
+}
---
+++ Assignment01_Receipes/Models/Recipe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment01_Receipes.Models
+{
+    public class Recipe
+    {
+        public string name;
+        public string description;
+    }
+}
